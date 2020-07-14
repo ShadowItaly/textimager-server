32f22c789c52f25314cc5c788582362d20da944d
Daniel Baumartz <baumartz@stud.uni-frankfurt.de>
2020-07-14 18:25:06 +0200

    fixes

--- ducc-agent/run.sh
+++ ducc-agent/run.sh
@@ -11,34 +11,28 @@ echo "ducc agent port: $TI_DOCKER_AGENT_PORT"
 # ssh
 service ssh start
 
-# allow login with this key
-cp /home/ducc/.ssh/id_rsa.pub /home/ducc/.ssh/authorized_keys
+DUCC_AGENT_INSTALLED="/home/ducc/.ducc_agent_installed"
+if [ ! -e $DUCC_AGENT_INSTALLED ]; then
+	echo "installing ducc agent..."
+	
+	# add head node ip to local hosts file
+	# TODO better manage with docker parameter?
+	echo $TI_DOCKER_HEAD_IP $TI_DOCKER_HEAD_HOST >> /etc/hosts
+	
+	# add head node to local ssh config to enable login on different port
+	echo "Host $TI_DOCKER_HEAD_HOST" >> /home/ducc/.ssh/config
+	echo "    HostName $TI_DOCKER_HEAD_IP" >> /home/ducc/.ssh/config
+	echo "    Port $TI_DOCKER_HEAD_PORT" >> /home/ducc/.ssh/config
+	echo "    IdentityFile ~/.ssh/id_rsa" >> /home/ducc/.ssh/config
+	chown -Rf ducc.ducc /home/ducc/.ssh/
 
-# disable required user input at first connect
-echo "StrictHostKeyChecking=no" > /home/ducc/.ssh/config
-
-# add head node to local ssh config to enable login on different port
-echo "Host $TI_DOCKER_HEAD_HOST" >> /home/ducc/.ssh/config
-echo "    HostName $TI_DOCKER_HEAD_IP" >> /home/ducc/.ssh/config
-echo "    Port $TI_DOCKER_HEAD_PORT" >> /home/ducc/.ssh/config
-echo "    IdentityFile ~/.ssh/id_rsa" >> /home/ducc/.ssh/config
-
-# modify access rights
-chmod 700 /home/ducc/.ssh
-chmod 600 /home/ducc/.ssh/id_rsa
-chmod +r /home/ducc/.ssh/id_rsa.pub
-chown -Rf ducc.ducc /home/ducc/.ssh/
-
-# use the same key for root user
-cp -Rf /home/ducc/.ssh/ /root/
-chown -Rf root.root /root/.ssh/
-
-# add head node ip to local hosts file
-# TODO better manage with docker parameter?
-echo $TI_DOCKER_HEAD_IP $TI_DOCKER_HEAD_HOST >> /etc/hosts
-
-# add this agent to head
-su - root -c "ssh -p $TI_DOCKER_HEAD_PORT root@$TI_DOCKER_HEAD_IP 'bash /home/ducc/ducc/add-node.sh $TI_DOCKER_AGENT_HOST $TI_DOCKER_AGENT_IP $TI_DOCKER_AGENT_PORT'"
+	# add this agent to head
+	ssh -p $TI_DOCKER_HEAD_PORT root@$TI_DOCKER_HEAD_IP "bash /home/ducc/ducc/add-node.sh $TI_DOCKER_AGENT_HOST $TI_DOCKER_AGENT_IP $TI_DOCKER_AGENT_PORT"
+	
+	touch $DUCC_AGENT_INSTALLED
+else
+	echo "ducc agent already installed"
+fi
 
 # daemonize Docker container
 while true; do sleep 1; done
--- ducc/run.sh
+++ ducc/run.sh
@@ -1,57 +1,22 @@
 #!/bin/sh
 
-# finishes the initialization of DUCC and starts the service
-
 # ssh 
 service ssh start
 
-# modify access rights
-chmod 700 /home/ducc/.ssh
-chmod 600 /home/ducc/.ssh/id_rsa
-chmod +r /home/ducc/.ssh/id_rsa.pub
-chown -Rf ducc.ducc /home/ducc/.ssh/
+DUCC_INSTALLED="/home/ducc/ducc/.ducc_installed"
+if [ ! -e $DUCC_INSTALLED ]; then
+	echo "installing ducc..."
+	
+	# DUCC post installation scripts
+	export LOGNAME="ducc"
+	su - ducc -c "cd /home/ducc/ducc/apache-uima-ducc/admin/ && /home/ducc/ducc/apache-uima-ducc/admin/ducc_post_install --head-node \"${TI_DOCKER_HEAD_HOST}\" --jvm \"/usr/local/openjdk-8/bin/java\""
+	
+	touch $DUCC_INSTALLED
+else
+	echo "ducc already installed"
+fi
 
-# allow login using this key
-cp /home/ducc/.ssh/id_rsa.pub /home/ducc/.ssh/authorized_keys
-
-# disable required user input at first connect
-echo "StrictHostKeyChecking=no" > /home/ducc/.ssh/config
-
-# use the same key for root user
-cp -Rf /home/ducc/.ssh/ /root/
-chown -Rf root.root /root/.ssh/
-
-# unpack UIMA DUCC
-mkdir /home/ducc/ducc/
-mkdir /home/ducc/ducc/apache-uima-ducc/
-cd /home/ducc/
-tar xzf uima-ducc-3.0.0-bin.tar.gz 
-mv apache-uima-ducc-3.0.0/* /home/ducc/ducc/apache-uima-ducc/
-rm -Rf /home/ducc/apache-uima-ducc-3.0.0/
-chown ducc.ducc -Rf /home/ducc/
-
-# DUCC post installation scripts
-export LOGNAME="ducc"
-cd /home/ducc/ducc/apache-uima-ducc/admin/
-INTERNAL_HOSTNAME=$(hostname)
-su - ducc -c "cd /home/ducc/ducc/apache-uima-ducc/admin/ && /home/ducc/ducc/apache-uima-ducc/admin/ducc_post_install --head-node \"$INTERNAL_HOSTNAME\" --jvm \"/usr/local/openjdk-8/bin/java\""
-
-# allow anonymous login to DUCC
-# TODO modify in future release
-mv /home/ducc/activemq-ducc.xml /home/ducc/ducc/apache-uima-ducc/apache-uima/apache-activemq/conf/activemq-ducc.xml
-
-# move other files...
-mv /home/ducc/add-node.sh /home/ducc/ducc/
-mv /home/ducc/DUCCServiceCreator.jar /home/ducc/ducc/
-mv /home/ducc/serviceScripts /home/ducc/ducc/
-mv /home/ducc/jars /home/ducc/ducc/
-
-# TODO ducc_ling
-#chown root.ducc /home/ducc/apache-uima-ducc/admin/ducc_ling
-#chmod 700 /home/ducc/apache-uima-ducc/admin/
-#chmod 4750 /home/ducc/apache-uima-ducc/admin/ducc_ling
-
-# finish installation
+# start ducc
 su - ducc -c "/home/ducc/ducc/apache-uima-ducc/admin/check_ducc"
 su - ducc -c "/home/ducc/ducc/apache-uima-ducc/admin/start_ducc"
 
