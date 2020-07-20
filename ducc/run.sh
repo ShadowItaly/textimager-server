cad5a1bbc3427730ee4cb4d366a45069f4d8be52
Wahed Hemati <hemati@em.uni-frankfurt.de>
2020-07-20 16:37:26 +0200

    permissions fixed

--- ducc/run.sh
+++ ducc/run.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 # ssh 
+chmod 700 /home/ducc/.ssh
+chmod 600 /home/ducc/.ssh/id_rsa
+chmod +r /home/ducc/.ssh/id_rsa.pub
+
 service ssh start
+
 
 DUCC_INSTALLED="/home/ducc/ducc/.ducc_installed"
 if [ ! -e $DUCC_INSTALLED ]; then
@@ -17,7 +22,9 @@ else
 fi
 
 # start ducc
+echo "checking ducc......"
 su - ducc -c "/home/ducc/ducc/apache-uima-ducc/admin/check_ducc"
+echo "starting ducc......"
 su - ducc -c "/home/ducc/ducc/apache-uima-ducc/admin/start_ducc"
 
 # daemonize Docker container
